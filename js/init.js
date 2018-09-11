e3fcf1e666791998eb5aac04cab57c6b7fa546c9
Martin <32832476+BXYMartin@users.noreply.github.com>
2018-09-11 14:59:28 +0800

    Add Main Structure

---
+++ js/init.js
@@ -0,0 +1,29 @@
+/*
+	Iridium by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+*/
+
+skel.init({
+	
+	breakpoints: {
+		'mobile': {
+			range: '-480',
+			lockViewport: true,
+			containers: 'fluid',
+			grid: {
+				collapse: true,
+				gutters: 10
+			}
+		},
+		'desktop': {
+			range: '481-',
+			containers: 1200
+		},
+		'1000px': {
+			range: '481-1200',
+			containers: 960
+		}
+	}
+}, {
+});
